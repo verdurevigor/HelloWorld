fe28833ecbaa53fdf55a1384e63ad5fc44da5b71
verdurevigor <brody.91@outlook.com>
2015-08-26 20:29:44 -0700

    Hello World
    First Hello World application draft

---
+++ Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace HelloWorld
+{
+    partial class HelloForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnShow = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.lblText = new System.Windows.Forms.Label();
+            this.gbColors = new System.Windows.Forms.GroupBox();
+            this.rbGreen = new System.Windows.Forms.RadioButton();
+            this.rbBlue = new System.Windows.Forms.RadioButton();
+            this.rbBlack = new System.Windows.Forms.RadioButton();
+            this.btnHide = new System.Windows.Forms.Button();
+            this.gbColors.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnShow
+            // 
+            this.btnShow.Location = new System.Drawing.Point(23, 24);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(76, 47);
+            this.btnShow.TabIndex = 0;
+            this.btnShow.Text = "Show";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(185, 24);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(76, 47);
+            this.btnClose.TabIndex = 1;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // lblText
+            // 
+            this.lblText.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblText.ForeColor = System.Drawing.Color.Black;
+            this.lblText.Location = new System.Drawing.Point(48, 159);
+            this.lblText.Name = "lblText";
+            this.lblText.Size = new System.Drawing.Size(186, 59);
+            this.lblText.TabIndex = 2;
+            this.lblText.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // gbColors
+            // 
+            this.gbColors.Controls.Add(this.rbGreen);
+            this.gbColors.Controls.Add(this.rbBlue);
+            this.gbColors.Controls.Add(this.rbBlack);
+            this.gbColors.Location = new System.Drawing.Point(12, 97);
+            this.gbColors.Name = "gbColors";
+            this.gbColors.Size = new System.Drawing.Size(258, 59);
+            this.gbColors.TabIndex = 3;
+            this.gbColors.TabStop = false;
+            this.gbColors.Text = "Font Color";
+            this.gbColors.Visible = false;
+            // 
+            // rbGreen
+            // 
+            this.rbGreen.AutoSize = true;
+            this.rbGreen.Location = new System.Drawing.Point(158, 21);
+            this.rbGreen.Name = "rbGreen";
+            this.rbGreen.Size = new System.Drawing.Size(69, 21);
+            this.rbGreen.TabIndex = 4;
+            this.rbGreen.Text = "Green";
+            this.rbGreen.UseVisualStyleBackColor = true;
+            this.rbGreen.CheckedChanged += new System.EventHandler(this.rbGreen_CheckedChanged);
+            // 
+            // rbBlue
+            // 
+            this.rbBlue.AutoSize = true;
+            this.rbBlue.Location = new System.Drawing.Point(95, 21);
+            this.rbBlue.Name = "rbBlue";
+            this.rbBlue.Size = new System.Drawing.Size(57, 21);
+            this.rbBlue.TabIndex = 5;
+            this.rbBlue.Text = "Blue";
+            this.rbBlue.UseVisualStyleBackColor = true;
+            this.rbBlue.CheckedChanged += new System.EventHandler(this.rbBlue_CheckedChanged);
+            // 
+            // rbBlack
+            // 
+            this.rbBlack.AutoSize = true;
+            this.rbBlack.Checked = true;
+            this.rbBlack.Location = new System.Drawing.Point(26, 21);
+            this.rbBlack.Name = "rbBlack";
+            this.rbBlack.Size = new System.Drawing.Size(63, 21);
+            this.rbBlack.TabIndex = 6;
+            this.rbBlack.TabStop = true;
+            this.rbBlack.Text = "Black";
+            this.rbBlack.UseVisualStyleBackColor = true;
+            this.rbBlack.CheckedChanged += new System.EventHandler(this.rbBlack_CheckedChanged);
+            // 
+            // btnHide
+            // 
+            this.btnHide.Location = new System.Drawing.Point(104, 24);
+            this.btnHide.Name = "btnHide";
+            this.btnHide.Size = new System.Drawing.Size(76, 47);
+            this.btnHide.TabIndex = 4;
+            this.btnHide.Text = "Hide";
+            this.btnHide.UseVisualStyleBackColor = true;
+            this.btnHide.Click += new System.EventHandler(this.btnHide_Click);
+            // 
+            // HelloForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Controls.Add(this.btnHide);
+            this.Controls.Add(this.gbColors);
+            this.Controls.Add(this.lblText);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnShow);
+            this.Name = "HelloForm";
+            this.Text = "Hello World";
+            this.gbColors.ResumeLayout(false);
+            this.gbColors.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnShow;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Label lblText;
+        private System.Windows.Forms.GroupBox gbColors;
+        private System.Windows.Forms.RadioButton rbGreen;
+        private System.Windows.Forms.RadioButton rbBlue;
+        private System.Windows.Forms.RadioButton rbBlack;
+        private System.Windows.Forms.Button btnHide;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Timers;
+
+namespace HelloWorld
+{
+    public partial class HelloForm : Form
+    {
+        public HelloForm()
+        {
+            InitializeComponent();
+
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            lblText.Text = "HELLO WORLD!";
+            gbColors.Visible = true;
+        }
+
+        private void rbBlack_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbBlack.Checked)
+                lblText.ForeColor = Color.Black;
+        }
+
+        private void rbBlue_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbBlue.Checked)
+                lblText.ForeColor = Color.Blue;
+        }
+
+        private void rbGreen_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbGreen.Checked)
+                lblText.ForeColor = Color.ForestGreen;
+        }
+
+        private void btnHide_Click(object sender, EventArgs e)
+        {
+            if (lblText.Text != "HELLO WORLD!")
+            {
+                MessageBox.Show("You cannot hide what is not shown.");
+            }
+            else
+            {
+                lblText.Text = "";
+                gbColors.Visible = false;
+            }
+        }
+    }
+}
